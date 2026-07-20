1f18a28bce6867fe9464c1d52c9730d1328c1c30
JanJasansky <jan.jasansky@unob.cz>
2026-07-20 09:57:45 +0200

    odstraneni_horniho panelu

--- apps/app_studyplan/src/AppRouter.jsx
+++ apps/app_studyplan/src/AppRouter.jsx
@@ -6,40 +6,25 @@ import {
 import { NavigationHistoryLinks, NavigationHistoryProvider } from '../../../packages/_template/src/Base/Helpers/NavigationHistoryProvider';
 import { BaseRouterSegments } from "../../../packages/_template/src/Base/Pages/RouterSegment";
 
-import { AppNavbar } from "./AppNavbar";
 import { StudyplanGQLModelRouterSegments } from "../../../packages/Study_Plan/src/StudyPlanGQLModel/Pages/RouterSegment";
-import { ProgramGQLModelRouterSegments } from "../../../packages/granting2/src/ProgramGQLModel/Pages/RouterSegment";
-import { StudentGQLModelRouterSegments } from "../../../packages/granting2/src/StudentGQLModel/Pages/RouterSegment";
-import { SemesterGQLModelRouterSegments } from "../../../packages/granting2/src/SemesterGQLModel/Pages/RouterSegment";
-import { TopicGQLModelRouterSegments } from "../../../packages/granting2/src/TopicGQLModel/Pages/RouterSegment";
-import { StudyPlanGQLModelRouterSegments } from "../../../packages/granting2/src/StudyPlanGQLModel/Pages/RouterSegment";
 
-// Společný layout pro všechny stránky appky: navigační lišta nahoře, pod ní historie
-// navigace (tlačítko zpět/breadcrumby) a `Outlet` je místo, kam react-router vloží
-// obsah aktuálně otevřené route (viz `children` v `Routes` níže)
+// Společný layout pro všechny stránky appky: pod historií navigace (tlačítko
+// zpět/breadcrumby) je `Outlet`, kam react-router vloží obsah aktuálně
+// otevřené route (viz `children` v `Routes` níže)
 const AppLayout = () => (
     <NavigationHistoryProvider>
-        <AppNavbar />
         <NavigationHistoryLinks />
         <Outlet />
     </NavigationHistoryProvider>
 );
 
-// Definice všech tras aplikace. `StudyplanGQLModelRouterSegments` (naše vlastní
-// route segmenty pro balíček Study_Plan, viz Pages/RouterSegment.jsx) jsou
-// záměrně první v poli, aby v případě shodné cesty s obecnými segmenty z
-// granting2 (StudyPlanGQLModelRouterSegments) měly přednost naše vlastní stránky.
+// Definice všech tras aplikace.
 const Routes = [
     {
         path: "/",
         element: <AppLayout />,
         children: [
             ...StudyplanGQLModelRouterSegments,
-            ...ProgramGQLModelRouterSegments,
-            ...StudentGQLModelRouterSegments,
-            ...SemesterGQLModelRouterSegments,
-            ...TopicGQLModelRouterSegments,
-            ...StudyPlanGQLModelRouterSegments,
             ...BaseRouterSegments,
         ],
     },
